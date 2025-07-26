d610a12380839f94e1d769e369a9badcfa667da2
Berkson Ximenes <berksonx@yahoo.com.br>
2025-07-25 22:04:56 -0300

    Adicionar pacote de validação e Criar CommitmentDto

---
+++ src/main/java/com/sof/sof_financeiro/model/BaseDto.java
@@ -0,0 +1,15 @@
+package com.sof.sof_financeiro.model;
+
+import lombok.*;
+
+/**
+ * Created By : Berkson Ximenes
+ * Date : 25/07/2025
+ **/
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BaseDto {
+    protected Long id;
+}
