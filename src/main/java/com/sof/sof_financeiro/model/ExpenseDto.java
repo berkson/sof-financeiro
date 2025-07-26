eeffa2f14417562df845265154939976a751e1fc
Berkson Ximenes <berksonx@yahoo.com.br>
2025-07-25 22:22:56 -0300

    Criar ExpenseDto

--- src/main/java/com/sof/sof_financeiro/model/ExpenseDto.java
+++ src/main/java/com/sof/sof_financeiro/model/ExpenseDto.java
@@ -1,9 +1,51 @@
 package com.sof.sof_financeiro.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.sof.sof_financeiro.enums.ExpenseType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * Created By : Berkson Ximenes
  * Date : 25/07/2025
  **/
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ExpenseDto {
+    @JsonProperty(value = "number")
+    private String protocolNumber;
+    @Enumerated(EnumType.STRING)
+    private ExpenseType expenseType;
+    @JsonProperty(value = "date")
+    @DateTimeFormat
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:MM:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime protocolDate;
+    @DateTimeFormat
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate expireDate;
+    private String creditor;
+    private String description;
+    private BigDecimal value;
 }
